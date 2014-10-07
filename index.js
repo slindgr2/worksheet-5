f4b650dd6a53cd78ceec3e3d1612966c97251eb9
Seth Lindgren <slindgr2@nebrwesleyan.edu>
2014-10-07 10:52:23 -0400

    problem 5

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 /*global console*/ // This line tells jsLint that you're allowed to use console.log for printing.
 
+ function SumOfNumbers(list) {
+  var result;
+  result = 0;
+  for (var index = 0; index < list.length; index = index + 1) {
+    result = result + list[index];
+  }
+  return result;
+}
+SumOfNumbers([1,2,3,4]);
+
